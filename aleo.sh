457aa30d4e8000b69e6c5587f6c348764806dd93
Aliez <renzholy@gmail.com>
2024-03-02 01:18:45 +0900

    Update aleo.sh

--- aleo.sh
+++ aleo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./snarkos start --nodisplay --prover --private-key $PROVER_PRIVATE_KEY & cpulimit -e snarkos --limit 85
+./snarkos start --nodisplay --prover --private-key $PROVER_PRIVATE_KEY & cpulimit -e snarkos --limit 80
